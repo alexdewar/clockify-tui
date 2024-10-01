fdae99fdab359f630a426a37399a318b9cc62743
Alex Dewar <a.dewar@imperial.ac.uk>
2024-10-01 11:13:54 +0100

    Add `tui` command
    We'll add more commands as we go along.

--- clockify_tui/__init__.py
+++ clockify_tui/__init__.py
@@ -1,6 +1,9 @@
 """The main module for Clockify TUI."""
 
+import sys
 from importlib.metadata import version
+
+import click
 
 from .ui import UI
 
@@ -9,5 +12,20 @@ __version__ = version(__name__)
 
 def main() -> None:
     """The main entry point to the program."""
+    # If the user doesn't supply a command, launch the TUI
+    if len(sys.argv) < 2:
+        tui()
+    else:
+        cli()
+
+
+@click.group()
+def cli() -> None:
+    """The top-level group containing all the commands for Clockify TUI."""
+
+
+@cli.command()
+def tui() -> None:
+    """Launch the TUI."""
     ui = UI()
     ui.run()
