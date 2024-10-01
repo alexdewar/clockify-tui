ff0c57a43def1fca99e8f0b477831ff9839a6faf
Alex Dewar <a.dewar@imperial.ac.uk>
2024-10-01 12:32:02 +0100

    Add command to test reading in config file

--- clockify_tui/__init__.py
+++ clockify_tui/__init__.py
@@ -8,6 +8,7 @@ import click
 from .ui import UI
 
 __version__ = version(__name__)
+from .config import read_config as read_config_file
 
 
 def main() -> None:
@@ -22,6 +23,14 @@ def main() -> None:
 @click.group()
 def cli() -> None:
     """The top-level group containing all the commands for Clockify TUI."""
+
+
+@cli.command()
+def read_config() -> None:
+    """A command to test reading in the config file."""
+    # Load the config file which includes the API key. Unused for now.
+    read_config_file()
+    print("Loaded config successfully")
 
 
 @cli.command()
---
+++ clockify_tui/config.py
@@ -0,0 +1,54 @@
+"""Code for loading and parsing the config file."""
+
+import tomllib
+from importlib import resources
+from pathlib import Path
+from shutil import copyfile
+
+from platformdirs import user_config_path
+from pydantic import BaseModel, ConfigDict
+
+
+class Clockify(BaseModel):
+    """The clockify section of the configuration file."""
+
+    api_key: str
+
+
+class Config(BaseModel):
+    """The program configuration."""
+
+    model_config = ConfigDict(from_attributes=True)
+    clockify: Clockify
+
+
+def _get_config_template_path() -> Path:
+    """Get the path to the config template file."""
+    return (
+        Path(str(resources.files("clockify_tui").joinpath()))
+        / "data"
+        / "config_template.toml"
+    )
+
+
+def _get_or_create_config_file_path() -> Path:
+    """Get the path to the config file, creating it if it doesn't exist.
+
+    The data/config_template.toml file will be used as a template.
+    """
+    path = user_config_path("clockify-tui", ensure_exists=True) / "config.toml"
+    if not path.exists():
+        template_path = _get_config_template_path()
+        copyfile(template_path, path)
+    return path
+
+
+def read_config() -> Config:
+    """Read the program's configuration file from the default location.
+
+    If the file does not exist, a default one will be created.
+    """
+    with _get_or_create_config_file_path().open("rb") as file:
+        config = tomllib.load(file)
+
+    return Config.model_validate(config)
