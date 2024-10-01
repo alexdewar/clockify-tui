40d8b2a65914ef2aa1d63ac20e15efb547aa9106
Alex Dewar <a.dewar@imperial.ac.uk>
2024-10-01 17:15:03 +0100

    Add infrastructure for registering keypresses and display in UI
    Closes #7.

--- clockify_tui/ui.py
+++ clockify_tui/ui.py
@@ -2,8 +2,20 @@
 
 import signal
 import sys
+from collections.abc import Callable
+from dataclasses import dataclass
 
 from blessed import Terminal
+
+
+@dataclass
+class KeypressHandler:
+    """Describes an action that can be invoked via a keypress."""
+
+    description: str
+    """A human-readable description of the action."""
+    action: Callable[[], None]
+    """The function to invoke."""
 
 
 class UI:
@@ -12,6 +24,34 @@ class UI:
     def __init__(self) -> None:
         """Create a new UI."""
         self._term = Terminal()
+        self._keypress_handlers: dict[str, KeypressHandler] = {}
+        self._should_quit = False
+
+        # Set up keyboard shortcuts
+        self.add_keypress_handler("q", "Quit the program", self.quit)
+
+    def quit(self) -> None:
+        """Exit the program."""
+        self._should_quit = True
+
+    def add_keypress_handler(
+        self,
+        key: str,
+        description: str,
+        action: Callable[[], None],
+    ) -> None:
+        """Add a keypress handler to the UI."""
+        self._keypress_handlers[key] = KeypressHandler(description, action)
+
+    def get_keypress_handler(self, key: str) -> None | Callable[[], None]:
+        """Get the keypress handler, if any, for the specified key.
+
+        If there is no handler registered, return None.
+        """
+        if handler := self._keypress_handlers.get(key):
+            return handler.action
+
+        return None
 
     def run(self) -> None:
         """Run the UI forever."""
@@ -25,15 +65,36 @@ class UI:
                     lambda sig, action: self._render(),
                 )
 
-            self._term.inkey()
+            while not self._should_quit:
+                key = self._term.inkey()
+                if handler := self.get_keypress_handler(key):
+                    handler()
 
     def _render(self) -> None:
-        print(self._term.home + self._term.clear, end="")
+        """The main render function."""
+        print(self._term.clear, end="")
 
+        # At bottom of screen
+        self._print_keyboard_shortcuts()
+
+        # Reset position to top
+        print(self._term.home, end="")
+
+        # Print title at top
         self._print_title()
 
-        print("Press any key to exit")
-
     def _print_title(self) -> None:
+        """Print the program's title."""
         txt = "Clockify TUI"
         print(f"{self._term.darkolivegreen}{self._term.center(txt)}{self._term.normal}")
+
+    def _print_keyboard_shortcuts(self) -> None:
+        """Print a list of keyboard shortcuts at the bottom of the screen."""
+        print(
+            self._term.blue
+            + self._term.move_y(self._term.height - len(self._keypress_handlers)),
+            end="",
+        )
+        for key, handler in self._keypress_handlers.items():
+            print(f"{key}: {handler.description}")
+        print(self._term.normal)
