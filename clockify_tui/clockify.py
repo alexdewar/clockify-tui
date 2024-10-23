57839ca2856ab1733f089f40e248b48dad02c589
Alex Dewar <a.dewar@imperial.ac.uk>
2024-10-23 08:58:21 +0100

    Fix: Don't try to load the config file on every code path
    Some commands, such as `edit-config` or `--help` need to be able to run without a config file present.

--- clockify_tui/clockify.py
+++ clockify_tui/clockify.py
@@ -2,14 +2,9 @@
 
 from clockify_api_client.client import ClockifyAPIClient
 
-from .config import try_read_config
-
-config = try_read_config()
-
-if config is None:
-    raise ValueError("Config file not found.")
-
-API_KEY = config.clockify.api_key
 API_URL = "api.clockify.me/v1"
 
-client = ClockifyAPIClient().build(API_KEY, API_URL)
+
+def get_client(api_key: str) -> ClockifyAPIClient:
+    """Get an API client using the specified API key."""
+    return ClockifyAPIClient().build(api_key, API_URL)
--- clockify_tui/workspaces.py
+++ clockify_tui/workspaces.py
@@ -1,31 +1,49 @@
 """Module for handling workspaces."""
 
-from .clockify import client
-from .config import try_read_config
+from clockify_api_client.models.workspace import Workspace
 
-workspaces = client.workspaces.get_workspaces()
+from .clockify import get_client
+from .config import Config, try_read_config
 
 
 def list_workspaces() -> None:
     """Get a list of available workspaces."""
+    config = try_read_config()
+    if not config:
+        return None
+
+    client = get_client(config.clockify.api_key)
+    workspaces = client.workspaces.get_workspaces()
     for workspace in workspaces:
         print(f"{workspace['name']}: {workspace['id']}")
+
+
+def get_selected_workspace(config: Config) -> Workspace | None:
+    """Get info about the user's selected workspace.
+
+    If no workspace is selected, the first one will be used.
+    """
+    client = get_client(config.clockify.api_key)
+    workspaces = client.workspaces.get_workspaces()
+
+    # If no workspace specified, just use the first one
+    if not config.clockify.workspace_id:
+        # **TODO**: Check there is at least one workspace?
+        return workspaces[0]
+
+    try:
+        return next(ws for ws in workspaces if ws["id"] == config.clockify.workspace_id)
+    except StopIteration:
+        print("Workspace not found.")
+        return None
 
 
 def workspace_info() -> None:
     """Get info about a specific workspace."""
     config = try_read_config()
-    if config is None:
-        raise ValueError("Config file not found.")
-    workspace_id = config.clockify.workspace_id or workspaces[0]["id"]
+    if not config:
+        return
 
-    workspace = next(
-        (workspace for workspace in workspaces if workspace["id"] == workspace_id),
-        None,
-    )
-
-    if workspace:
+    if workspace := get_selected_workspace(config):
         for key, value in workspace.items():
             print(f"{key}: {value}")
-    else:
-        print("Workspace not found.")
