b187e2a6612170966009760a857ab2f5936a1bf7
Alex Dewar <a.dewar@imperial.ac.uk>
2024-11-04 08:29:32 +0000

    Refactor function for getting selected/default workspace

--- clockify_tui/workspaces.py
+++ clockify_tui/workspaces.py
@@ -1,9 +1,7 @@
 """Module for handling workspaces."""
 
-from clockify_api_client.models.workspace import Workspace
-
-from .clockify import get_client
-from .config import Config, try_read_config
+from .clockify import get_client, get_selected_workspace_or_default
+from .config import try_read_config
 
 
 def list_workspaces() -> None:
@@ -18,33 +16,16 @@ def list_workspaces() -> None:
         print(f"{workspace['name']}: {workspace['id']}")
 
 
-def get_selected_workspace(config: Config) -> Workspace | None:
-    """Get info about the user's selected workspace.
-
-    If no workspace is selected, the first one will be used.
-    """
-    client = get_client(config.clockify.api_key)
-    workspaces = client.workspaces.get_workspaces()
-
-    # If no workspace specified, just use the first one
-    if not config.clockify.workspace_id:
-        # **TODO**: Check there is at least one workspace?
-        return workspaces[0]
-
-    try:
-        return next(ws for ws in workspaces if ws["id"] == config.clockify.workspace_id)
-    except StopIteration:
-        print("Workspace not found.")
-        return None
-
-
 def workspace_info() -> None:
     """Get info about a specific workspace."""
     config = try_read_config()
     if not config:
         return
+    client = get_client(config.clockify.api_key)
 
-    if workspace := get_selected_workspace(config):
+    if workspace := get_selected_workspace_or_default(
+        client, config.clockify.workspace_id
+    ):
         for key, value in workspace.items():
             print(f"{key}: {value}")
 
